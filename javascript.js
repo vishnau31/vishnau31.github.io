330323f16f2c1153a7ccad2768483ffdcc4d6c65
Khushbu0304 <37973552+Khushbu0304@users.noreply.github.com>
2020-07-28 20:53:20 +0530

    added styling.css

--- javascript.js
+++ javascript.js
@@ -1,87 +1,159 @@
+var playing=false;
+var score;
+var action;
+var timeremaining;
+var correctAnswer;
 
-var a;
-var i;
-var gameCounter=0;;
-var x;
-var mul;
-var option = ['','','',''];
-var score=0;
-var mc;
-function valueSet()
-{
-	a=0;
-	document.getElementById("startreset").innerHTML="Start Game";
-}
-function next(){
-      if(gameCounter<=150)
-      {
-      var no1= Math.round(Math.random()*10);
-	  var no2= Math.round(Math.random()*10);
-	  var ques=no1+"+"+no2;
-	  mul=no1+no2;
-	 
-	  var randIndex = Math.round(Math.random()*3);
-	  option[randIndex]=mul;
-	  
-	  for(i=0;i<=3;i++)
-	  {
-	  	if(i==randIndex)
-	  		continue;
-	  	option[i]=Math.round(Math.random()*100);
 
-	  }
-	  for(i=0;i<=3;i++)
-	  {
-	    var boxno="box"+(i+1);
-	    document.getElementById(boxno).innerHTML=option[i];
-	  }
-      document.getElementById("question").innerHTML=ques;
-       x=10;
-	  clearInterval(mc);
-	  var m=document.getElementById("trv");
-	  mc=setInterval(
-	  	function()
-	  	{x--;m.innerHTML=x;if(x<=0){next();}},1000);
-	  gameCounter++;
-	 }
-	 else{
 
-	 	document.getElementById("gameOver").style.display="inline";
-	 	document.getElementById("final").innerHTML=score;
-        
-	 }
-	 
-}
-	function startGame()
-{
-	if(a==0)
-	{ 
-	  document.getElementById("startreset").innerHTML="Reset Game";
-	  a=1;
-	  document.getElementById("timeremaining").style.display="inline";
-	  next();
+document.getElementById("startreset").onclick=function(){
+	if(playing==true)
+	{
+       location.reload();
 	}
 	else
 	{
-	  location.reload();
+	   playing=true;
+       score=0;
+       document.getElementById("scorevalue").innerHTML=score;
+       show("timeremaining");
+       timeremaining=10;
+       document.getElementById("timeremainingvalue").innerHTML=timeremaining;
+       hide("gameover");
+
+       document.getElementById("startreset").innerHTML="Reset Game";
+
+       
+       startCountdown();
+
+
+       //generate new question and multiple ans
+
+       generateQA();
+       
+    }
+}
+
+
+//clicking answer box
+for(i=1;i<5;i++)
+{
+document.getElementById("box"+i).onclick=function(){
+//check if we are playing
+if(playing==true)
+{
+	if(this.innerHTML==correctAnswer)
+	{
+      //correct answer
+      score++;
+      document.getElementById("scorevalue").innerHTML=score;
+      //hide wrong box and show correct box
+      hide("wrong");
+      show("correct");
+      setTimeout(function(){
+      hide("correct");
+
+      },1000);
+      generateQA();
+      
+	}
+
+    else
+    {
+	hide("correct");
+	show("wrong");
+	setTimeout(function(){
+		hide("wrong");
+		},1000);
 	}
 }
-function check(a)
-{
- if(option[a]==mul)
-   {
- 	document.getElementById("scorevalue").innerHTML=(++score);
- 	document.getElementById("correct").style.display="inline"
-    var cor=setTimeout(function(){document.getElementById("correct").style.display="none"},0);
-    next();
-   }
-  else
-  {
-  	document.getElementById("wrong").style.display="inline"
-    var wro=setTimeout(function(){document.getElementById("wrong").style.display="none"},0);
-  	document.getElementById("wrong").style.display="inline";
-  	
-  	next();
-    
-  }
 }
+}
+
+//functions
+
+
+//startcounter
+function startCountdown(){
+	action=setInterval(function(){
+    timeremaining-=1; 
+    document.getElementById("timeremainingvalue").innerHTML=timeremaining; 
+	if(timeremaining==0)
+	{
+		//game over
+		stopCountdown();
+		show("gameover");
+		document.getElementById("gameover").innerHTML="<p>Game Over</p><p>your score is "+score+".</p>";
+		hide("timeremaining");
+		hide("correct");
+		hide("wrong");
+		playing=false;
+		document.getElementById("startreset").innerHTML="Start Game";
+		  
+	}
+	},1000);
+}
+
+
+//stopcounter
+function stopCountdown()
+{
+	clearInterval(action);//stopcounter if game is over
+}
+
+
+
+//show
+function show(Id)
+{
+	document.getElementById(Id).style.display="block";
+}
+
+
+//hide an element
+function hide(Id)
+{
+	document.getElementById(Id).style.display="none";
+}
+
+
+
+//generate q n a
+
+function generateQA(){
+	var x=1+Math.round(10* Math.random());
+	var y=1+Math.round(10* Math.random());
+	correctAnswer=x+y;
+    document.getElementById("question").innerHTML=x+"+"+y;
+    var correctPosition=1+Math.round(3* Math.random());
+    //correct option box
+    document.getElementById("box"+correctPosition).innerHTML=correctAnswer;
+    //wrong answer boxes
+
+  var answers=[correctAnswer];  
+
+
+for(i=1;i<5;i++){
+	if(i!=correctPosition)
+	{
+		var wrongAnswer;
+		do
+		{
+			wrongAnswer=(1+Math.round(10* Math.random()))*(1+Math.round(10* Math.random()));
+		}
+		while(answers.indexOf(wrongAnswer)>-1);
+		
+
+      document.getElementById("box"+i).innerHTML=wrongAnswer;
+      answers.push(wrongAnswer);
+
+
+    }
+}
+
+}
+
+
+
+
+
